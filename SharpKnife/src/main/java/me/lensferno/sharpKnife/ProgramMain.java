e7baf1f6d462056d4d0f51f7819237c81a3a6464
lensferno <lensferno@outlook.com>
2021-06-17 21:19:43 +0800

    first commit

---
+++ SharpKnife/src/main/java/me/lensferno/sharpKnife/ProgramMain.java
@@ -0,0 +1,16 @@
+package me.lensferno.sharpKnife;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ProgramMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    static void main(String[] args) {
+        launch(args);
+    }
+
+}
